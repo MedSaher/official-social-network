75c4e6c2012785853990955b685ea7bcdadb864e
Mossab Idbenkellal <idmossab@gmail.com>
2025-07-20 13:01:51 +0100

    edit user repo in domain

--- backend/internal/domain/ports/repository/user_repository.go
+++ backend/internal/domain/ports/repository/user_repository.go
@@ -1,8 +1,11 @@
 package repository
 
-import "social_network/internal/domain/models"
+import (
+	"social_network/internal/domain/models"
+)
 
 type UserRepository interface {
 	RegisterNewUser(user *models.User) error
-	GetUserByUsername(username string) (*models.User, error)
+	GetUserByEmail(email string) (*models.User, error)
+	GetUserByID(id int) (*models.User, error)
 }
