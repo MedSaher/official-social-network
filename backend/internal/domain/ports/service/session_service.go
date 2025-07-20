af125cf57c8eadaf93ab2e11ea1dc0ed3455374b
Mossab Idbenkellal <idmossab@gmail.com>
2025-07-20 13:36:12 +0100

    create service session

--- backend/internal/domain/ports/service/session_service.go
+++ backend/internal/domain/ports/service/session_service.go
@@ -1,1 +1,10 @@
 package service
+
+import "time"
+
+type SessionServicePort interface {
+	CreateSession(userID int) (string, time.Time, error)
+	DestroySession(token string) error
+	IsValidSession(token string) bool
+	GetUserIdFromSession(token string) (int, error)
+}
