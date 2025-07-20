84ef394a1fe5791bd37294fa8010b05e6c5f868d
Mossab Idbenkellal <idmossab@gmail.com>
2025-07-20 12:23:13 +0100

    change model user

--- backend/internal/domain/models/user.go
+++ backend/internal/domain/models/user.go
@@ -1,15 +1,17 @@
 package models
 
 type User struct {
-	Id          int   `json:"id"`
-	NickName    string `json:"nickname"`
-	Username    string `json:"username"`
-	Age         int    `json:"age"`
-	DateOfBirth string `json:"dateOfBirth"`
-	Gender      string `json:"gender"`
-	FirstName   string `json:"firstName"`
-	LastName    string `json:"lastName"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	About       string `json:"aboutMe"`
+	Id           int     `json:"id"`
+	Nickname     string  `json:"nickname"`
+	Username     string  `json:"username"`
+	DateOfBirth  string  `json:"dateOfBirth"` // use string or time.Time
+	Gender       string  `json:"gender"`
+	PasswordHash string  `json:"password"`    // stored hashed
+	Email        string  `json:"email"`
+	FirstName    string  `json:"firstName"`
+	LastName     string  `json:"lastName"`
+	AvatarPath   *string `json:"avatarUrl"`   // nullable
+	AboutMe      *string `json:"aboutMe"`     // nullable
+	IsPublic     bool    `json:"privacyStatus"`
+	CreatedAt    string  `json:"createdAt"`   // optional, for info
 }
