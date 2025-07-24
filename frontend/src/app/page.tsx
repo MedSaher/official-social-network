3fd210c6b82eeb824b0455988869b0438dbe9c12
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-07-24 05:55:56 +0100

    user loging and registration ENHANCED

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import RenderHomePage from "../components/home"
 import axios from 'axios'
 
 export default function HomePage() {
@@ -22,22 +23,10 @@ export default function HomePage() {
     checkSession()
   }, [])
 
-  const handleLogout = async () => {
-    try {
-      await axios.post('http://localhost:8080/api/logout', {}, {
-        withCredentials: true, // ✅ Include session_token in logout request
-      })
-    } catch (error) {
-      console.error('Failed to logout:', error)
-    } finally {
-      router.push('/login')
-    }
-  }
+    return (
+      <main>
+        <RenderHomePage />
+      </main>
+    );
 
-  return (
-    <div>
-      <h1>Welcome to Home Page</h1>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  )
 }
---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import axios from "axios"
+import { useRouter } from 'next/navigation'
+
+export default function RenderHomePage() {
+    let router = useRouter()
+      const handleLogout = async () => {
+    try {
+      await axios.post('http://localhost:8080/api/logout', {}, {
+        withCredentials: true, // ✅ Include session_token in logout request
+      })
+    } catch (error) {
+      console.error('Failed to logout:', error)
+    } finally {
+      router.push('/login')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Welcome to Home Page</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
+}
