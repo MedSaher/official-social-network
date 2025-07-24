ffe642b08dc08082c8e2ac61747419a02ca20ae2
Mossab Idbenkellal <idmossab@gmail.com>
2025-07-24 16:17:36 +0100

    create model profil

---
+++ backend/internal/domain/models/profil.go
@@ -0,0 +1,9 @@
+package models
+
+type FullProfileResponse struct {
+	User           UserProfileDTO `json:"user"`
+	FollowersCount int            `json:"followers_count"`
+	FollowingCount int            `json:"following_count"`
+	Followers      []FollowerInfo `json:"followers"`
+	Following      []FollowerInfo `json:"following"`
+}
