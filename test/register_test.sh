d1a93bee44838e31c65510695fd9ac6050d7d2f2
Mossab Idbenkellal <idmossab@gmail.com>
2025-07-24 11:59:11 +0100

    edit script register

--- test/register_test.sh
+++ test/register_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-curl -X POST http://localhost:8080/api/register \
+curl -s -X POST http://localhost:8080/api/register \
   -H "Content-Type: application/json" \
   -d '{
-    "nickname": "mossab93",
     "username": "mossab_user",
     "email": "mossab@example.com",
     "password": "strongpassword123",
-    "firstName": "mossab",
-    "lastName": "mossab",
-    "gender": "Male",
+    "firstName": "Mossab",
+    "lastName": "Lahbib",
+    "gender": "male",
     "dateOfBirth": "1993-08-15",
-    "aboutMe": "Je suis passionné de code et de backend Go."
+    "aboutMe": "Je suis passionné de code et de backend Go.",
+    "privacyStatus": "public"
   }'
